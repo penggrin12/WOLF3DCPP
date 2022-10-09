6eeec2704141a2e8d74dfe48295e7b64f0287460
Penggrin <miner.sidor@gmail.com>
2022-10-09 16:53:27 +0300

    Removed older files

--- WOLF3DCPP3/textures.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Textures/All_Textures.ppm"
-#include "Textures/sky.ppm"
-#include "Textures/title.ppm"
-#include "Textures/won.ppm"
-#include "Textures/lost.ppm"
-#include "Textures/sprites.ppm"
